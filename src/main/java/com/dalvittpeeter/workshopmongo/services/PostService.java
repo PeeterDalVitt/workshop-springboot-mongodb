7ad44caafc313f3d29959a90403a0fa9b7907998
Peeter Dal Vitt <peeter1301@gmail.com>
2025-06-10 11:40:43 -0300

    Obtendo um post por id

---
+++ src/main/java/com/dalvittpeeter/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.dalvittpeeter.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dalvittpeeter.workshopmongo.domain.Post;
+import com.dalvittpeeter.workshopmongo.repository.PostRepository;
+import com.dalvittpeeter.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
